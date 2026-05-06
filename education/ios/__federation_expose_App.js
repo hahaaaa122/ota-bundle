40d6dce683f397585f7fb6e48361cbf22fd899da
Frank_gallagher <83605669+hahaaaa122@users.noreply.github.com>
2026-05-06 15:50:24 +0700

    deploy(education): chunk __federation_expose_App.js

--- education/ios/__federation_expose_App.js
+++ education/ios/__federation_expose_App.js
@@ -9,11 +9,96 @@ Object.defineProperty(exports, "default", ({
         return EducationRemoteApp;
     }
 }));
-var _interop_require_wildcard = __webpack_require__("../../node_modules/.pnpm/@swc+helpers@0.5.21/node_modules/@swc/helpers/cjs/_interop_require_wildcard.cjs");
-var _sliced_to_array = __webpack_require__("../../node_modules/.pnpm/@swc+helpers@0.5.21/node_modules/@swc/helpers/cjs/_sliced_to_array.cjs");
 var _jsxruntime = __webpack_require__("../../node_modules/.pnpm/react@19.2.3/node_modules/react/jsx-runtime.js");
-var _react = /*#__PURE__*/ _interop_require_wildcard._(__webpack_require__("webpack/sharing/consume/default/react/react"));
+var _react = /*#__PURE__*/ _interop_require_wildcard(__webpack_require__("webpack/sharing/consume/default/react/react"));
 var _reactnative = __webpack_require__("webpack/sharing/consume/default/react-native/react-native");
+function _array_like_to_array(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
+    return arr2;
+}
+function _array_with_holes(arr) {
+    if (Array.isArray(arr)) return arr;
+}
+function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function(nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}
+function _interop_require_wildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || typeof obj !== "object" && typeof obj !== "function") {
+        return {
+            default: obj
+        };
+    }
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {
+        __proto__: null
+    };
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+function _iterable_to_array_limit(arr, i) {
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
+    if (_i == null) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _s, _e;
+    try {
+        for(_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true){
+            _arr.push(_s.value);
+            if (i && _arr.length === i) break;
+        }
+    } catch (err) {
+        _d = true;
+        _e = err;
+    } finally{
+        try {
+            if (!_n && _i["return"] != null) _i["return"]();
+        } finally{
+            if (_d) throw _e;
+        }
+    }
+    return _arr;
+}
+function _non_iterable_rest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _sliced_to_array(arr, i) {
+    return _array_with_holes(arr) || _iterable_to_array_limit(arr, i) || _unsupported_iterable_to_array(arr, i) || _non_iterable_rest();
+}
+function _unsupported_iterable_to_array(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _array_like_to_array(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(n);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
+}
 var COURSES = [
     {
         id: '1',
@@ -118,7 +203,7 @@ var Text = (ref)=>{
 };
 function EducationRemoteApp(ref) {
     var onBack = ref.onBack;
-    var _useState = _sliced_to_array._((0, _react.useState)('enrolled'), 2), activeTab = _useState[0], setActiveTab = _useState[1];
+    var _useState = _sliced_to_array((0, _react.useState)('enrolled'), 2), activeTab = _useState[0], setActiveTab = _useState[1];
     var enrolledCourses = COURSES.filter((c)=>c.progress > 0);
     var displayCourses = activeTab === 'enrolled' ? enrolledCourses : COURSES;
     return /*#__PURE__*/ (0, _jsxruntime.jsxs)(_reactnative.ScrollView, {
